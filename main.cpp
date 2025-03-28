26e9a43f66e2e0e29875907f15d5ed1f06ae9132
TienMinhDuong <tmd31052005@gmail.com>
2025-03-28 14:21:39 +0700

    Xóa 2 dòng

--- main.cpp
+++ main.cpp
@@ -6,8 +6,4 @@ int main()
     cout << "Hello, World!" << endl;
     cout << "This is a test." << endl;
     cout << "This is only a test." << endl;
-    cout << "This is a test of the emergency broadcast system." << endl;
-    cout << "If this had been an actual emergency, you would have been instructed where to tune in your area for news and official information." << endl;
-    cout << "This concludes this test of the emergency broadcast system." << endl;
-    return 0;
 }
