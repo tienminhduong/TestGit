3a632a9aef20c494ba7fd9a3cb48d04f08756030
TienMinhDuong <tmd31052005@gmail.com>
2025-03-28 14:28:21 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -6,4 +6,5 @@ int main()
     cout << "Hello, World!" << endl;
     cout << "This is a test." << endl;
     cout << "This is only a test." << endl;
+    cout << "This is a test of the emergency broadcast system." << endl;
 }
